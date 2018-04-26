d33b37910b62af36dcffaaf0a26d3da7c547312d
shalvah <diakon.ng@gmail.com>
2018-04-26 02:06:16 +0100

    Add spreadsheet routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('sheets/new', 'SheetsController@newSheet')->name('sheets.new');
+Route::get('sheets/{sheet}', 'SheetsController@view')->name('sheets.view');
+Route::put('sheets/{id}', 'SheetsController@update');
