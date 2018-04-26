986c66f68465ccb66669cea79ec719f6baa82a23
shalvah <diakon.ng@gmail.com>
2018-04-26 21:11:08 +0100

    Add `channel_name` accessor for sheets

--- app/Models/Sheet.php
+++ app/Models/Sheet.php
@@ -7,4 +7,9 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class Sheet extends Model
 {
     protected $guarded = [];
+
+    public function getChannelNameAttribute()
+    {
+        return "presence-sheet-$this->_id";
+    }
 }
